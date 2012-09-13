b69a8dba81ba16742af1aed906b4ccc03094e717
Adam Auckland <adamauckland@gmail.com>
2012-09-13 08:31:45 +0100

    refactored

--- jsorm.js
+++ jsorm.js
@@ -1,6 +1,8 @@
  
 
 var JSORM = {
+	Initialised: false,
+
 	Database: function(initialiser) {
 		this.config = initialiser;
 		var self = this;
@@ -9,8 +11,6 @@ var JSORM = {
 
 		};
 
-		
-		
 		this.openDatabase = function() {
 			self.DB = window.openDatabase(
 				self.config.name,
@@ -20,6 +20,10 @@ var JSORM = {
 				self.createDatabase);
 			return self.DB;
 		};
+
+		if(!JSORM.Initialised) {
+			JSORM.Model.prototype.manager = JSORM.manager;
+		}
 	},
 
 
@@ -52,6 +56,163 @@ var JSORM = {
 
 
 
+	manager: {
+		create: function() {
+			var data = {};
+			this.model.attachedFields.forEach(
+				function(__loopField) {
+					data[__loopField.name] = null;
+				}
+			);
+
+			var result = new JSORM.LiveObject(data);
+			result.model = this.model;
+
+			return result;
+		},
+
+
+		buildSelect: function(criteria) {
+			var sqlScript = [];
+			var queryParams = [];
+			var model = this.model;
+			
+			sqlScript.push('SELECT ');
+
+			for(var fieldIndex = 0; fieldIndex < model.attachedFields.length; fieldIndex++) {
+				if(fieldIndex > 0) {
+					sqlScript.push(', ');
+				}
+				sqlScript.push('[' + model.attachedFields[fieldIndex].name + ']');
+			}
+
+			sqlScript.push(' FROM [' + model.config.name + ']');
+
+			if(criteria ) {
+				sqlScript.push(' WHERE ');
+
+				var operator;
+				var fieldName;
+				var value;
+
+				for(var criteriaItem in criteria) {
+					fieldName = criteriaItem;
+					operator = '=';
+					value = criteria[criteriaItem];
+					//
+					// double underscores are control codes to modify the operator.
+					// Use them after the fieldname to modify how you want to retrieve the data.
+					//
+					// options are:  lessthan, greaterthan, not, contains
+					//
+					// example:    id__lessthan: 5
+					//
+					// or: name__contains: 'fred'
+					//
+					if(criteriaItem.indexOf('__') != -1) {
+						switch(criteriaItem.split('__')[1].toLowerCase()) {
+							case 'lessthan':
+								operator = '<';
+								break;
+
+							case 'greaterthan':
+								operator = '>';
+								break;
+
+							case 'not':
+								operator = '<>';
+								break;
+
+							case 'contains':
+								operator = ' like ';
+								value = '%' + value + '%';
+								break;
+
+						}
+
+						fieldName = fieldName.split('__')[0];
+					}
+
+					sqlScript.push('[' + fieldName + ']' + operator + '?');
+					queryParams.push(value);
+				}
+			}
+			return [sqlScript.join(' '), queryParams];
+		},
+
+
+		get: function(criteria, successCallback, failureCallback) {
+			var model = this.model;
+			var buildSelect = model.manager.buildSelect(criteria);
+			var commandToExecute = buildSelect[0];
+			var queryParams = buildSelect[1];
+			var result = null;
+		
+			model.database.transaction( function(tx) {
+				console.log(commandToExecute);
+				console.log(queryParams);
+
+				tx.executeSql(
+					commandToExecute,
+					queryParams,
+					function(t, data) {
+						if(data.rows.length == 1) {
+							result = new JSORM.LiveObject(data.rows.item(0));
+							result.model = model;
+						}
+						if(data.rows.length > 1) {
+							throw new Exception('Query returned more than one result');
+						}
+					},
+					function(t, error) {
+						
+					}
+				);
+			}, function(a, error) {
+				failureCallback(error);
+			}, function() {
+				successCallback(result);
+			});
+		},
+
+
+		getAll: function(criteria, successCallback, failureCallback) {
+			var model = this.model;
+			var buildSelect = model.manager.buildSelect(criteria);
+			var commandToExecute = buildSelect[0];
+			var queryParams = buildSelect[1];
+			var result = [];
+			
+		
+			model.database.transaction( function(tx) {
+				console.log(commandToExecute);
+				console.log(queryParams);
+
+				tx.executeSql(
+					commandToExecute,
+					queryParams,
+					function(t, data) {
+						for(var i = 0; i < data.rows.length; i++) {
+							var newData = new JSORM.LiveObject(data.rows.item(i));
+							newData.model = model;
+							result.push(newData);
+						}
+					},
+					function(t, error) {
+						
+					}
+				);
+			}, function(a, error) {
+				failureCallback(error);
+			}, function() {
+				successCallback(result);
+			});
+		}
+	},
+
+
+
+
 	Model: function(database, name, config) {
 		this.config = {
 			name: name,
@@ -61,9 +222,9 @@ var JSORM = {
 		this.database = database;
 		this.attachedFields = [];
 
-		var modelThis = this;
+		var model = this;
 		this.attachField = function(item) {
-			modelThis.attachedFields.push(item);
+			model.attachedFields.push(item);
 		};
 
 
@@ -74,293 +235,153 @@ var JSORM = {
 			//
 			// default to id
 			//
-			modelThis.attachField(
-				new JSORM.FieldTypes.IntegerField(modelThis.config.id, false)
+			model.attachField(
+				new JSORM.FieldTypes.IntegerField(model.config.id, false)
 			);
 		}
 
+		this.manager.model = this;
+	},
+
+
+
+	LiveObject: function(data) {
+		var liveObjectThis = this;
 		
-		this.LiveObject = function(data) {
-			var liveObjectThis = this;
-			this.__original_data = data;
+		this.__original_data = data;
 
-			for(var item in data) {
-				this[item] = data[item];
-			}
+		for(var item in data) {
+			this[item] = data[item];
+		}
 
 
-			this.remove = function(successCallback, failureCallback) {
-				var sqlScript = [];
-				var queryParams = [];
+		this.remove = function(successCallback, failureCallback) {
+			var sqlScript = [];
+			var queryParams = [];
+			var model = liveObjectThis.model;
 
-				sqlScript.push('DELETE FROM [' + modelThis.config.name + '] ' );
-				sqlScript.push(' WHERE ');
+			sqlScript.push('DELETE FROM [' + model.config.name + '] ' );
+			sqlScript.push(' WHERE ');
 
-				sqlScript.push('[' + modelThis.config.id + ']=? ');
-				queryParams.push(liveObjectThis[modelThis.config.id]);
+			sqlScript.push('[' + model.config.id + ']=? ');
+			queryParams.push(liveObjectThis[model.config.id]);
 
-				var commandToExecute = sqlScript.join('');
+			var commandToExecute = sqlScript.join('');
 
-				modelThis.database.transaction( function(tx) {
-					console.log(commandToExecute);
-					console.log(queryParams);
+			model.database.transaction( function(tx) {
+				console.log(commandToExecute);
+				console.log(queryParams);
 
-					tx.executeSql(
-						commandToExecute,
-						queryParams,
-						function(t, data) {
-							if(data.rowsAffected == 1) {
-								
-							}
-						},
-						function(t, error) {
+				tx.executeSql(
+					commandToExecute,
+					queryParams,
+					function(t, data) {
+						if(data.rowsAffected == 1) {
 							
 						}
-					);
-				}, function(a, error) {
-					// transaction fail
-					failureCallback(error);
-				}, function() {
-					// success
-					successCallback();
-				});
-			};
-
-
-			this.save = function(successCallback, failureCallback) {
-				var __changedFields = [];
-				var sqlScript = [];
-				var queryParams = [];
-				var changed = false;
-
-				if(liveObjectThis[modelThis.config.id] === null) {
-					changed = true;
-					//
-					// INSERT
-					//
-					sqlScript.push('INSERT INTO [' + modelThis.config.name + '] (' );
-					//
-					// fieldindex 0 is always the ID
-					//
-					for(var fieldIndex = 1; fieldIndex < modelThis.attachedFields.length; fieldIndex++) {
-						if(fieldIndex > 1) {
-							sqlScript.push(', ');
-						}
-						sqlScript.push('[' + modelThis.attachedFields[fieldIndex].name + ']');
+					},
+					function(t, error) {
+						
 					}
-					sqlScript.push(') VALUES (');
-
-					for(fieldIndex = 1; fieldIndex < modelThis.attachedFields.length; fieldIndex++) {
-						if(fieldIndex > 1) {
-							sqlScript.push(', ');
-						}
-						sqlScript.push(' ? ');
-						queryParams.push(liveObjectThis[modelThis.attachedFields[fieldIndex].name]);
-					}
-	
-					sqlScript.push(');');
-				} else {
-					//
-					// UPDATE
-					//
-					sqlScript.push('UPDATE [' + modelThis.config.name + '] SET ' );
-
-					modelThis.attachedFields.forEach(
-						function(__loopField) {
-							//
-							// check if field has changed
-							//
-							if(liveObjectThis[__loopField.name] != liveObjectThis.__original_data[__loopField.name]) {
-								sqlScript.push('[' + __loopField.name + ']=?');
-								queryParams.push(liveObjectThis[__loopField.name]);
-								changed = true;
-							}
-						}
-					);
-
-					sqlScript.push(' WHERE [' + modelThis.config.id + ']=?');
-					queryParams.push(liveObjectThis[modelThis.config.id]);
-				}
-				//
-				// nothing to save
-				//
-				if(!changed) {
-					successCallback();
-					return;
-				}
-
-				var commandToExecute = sqlScript.join('');
-
-				modelThis.database.transaction( function(tx) {
-					console.log(commandToExecute);
-					console.log(queryParams);
-
-					tx.executeSql(
-						commandToExecute,
-						queryParams,
-						function(t, data) {
-							if(data.rowsAffected == 1) {
-								if(data.insertId !== undefined) {
-									liveObjectThis[modelThis.config.id] = data.insertId;
-								}
-							}
-						},
-						function(t, error) {
-							
-						}
-					);
-				}, function(a, error) {
-					failureCallback(error);
-				}, function() {
-					successCallback();
-				});
-			};
+				);
+			}, function(a, error) {
+				// transaction fail
+				failureCallback(error);
+			}, function() {
+				// success
+				successCallback();
+			});
 		};
 
 
-		this.manager = {
-			create: function() {
-				var data = {};
-				modelThis.attachedFields.forEach(
+		this.save = function(successCallback, failureCallback) {
+			var __changedFields = [];
+			var sqlScript = [];
+			var queryParams = [];
+			var changed = false;
+			var model = liveObjectThis.model;
+
+			if(liveObjectThis[liveObjectThis.model.config.id] === null) {
+				changed = true;
+				//
+				// INSERT
+				//
+				sqlScript.push('INSERT INTO [' + model.config.name + '] (' );
+				//
+				// fieldindex 0 is always the ID
+				//
+				for(var fieldIndex = 1; fieldIndex < model.attachedFields.length; fieldIndex++) {
+					if(fieldIndex > 1) {
+						sqlScript.push(', ');
+					}
+					sqlScript.push('[' + model.attachedFields[fieldIndex].name + ']');
+				}
+				sqlScript.push(') VALUES (');
+
+				for(fieldIndex = 1; fieldIndex < model.attachedFields.length; fieldIndex++) {
+					if(fieldIndex > 1) {
+						sqlScript.push(', ');
+					}
+					sqlScript.push(' ? ');
+					queryParams.push(liveObjectThis[model.attachedFields[fieldIndex].name]);
+				}
+
+				sqlScript.push(');');
+			} else {
+				//
+				// UPDATE
+				//
+				sqlScript.push('UPDATE [' + model.config.name + '] SET ' );
+
+				model.attachedFields.forEach(
 					function(__loopField) {
-						data[__loopField.name] = null;
+						//
+						// check if field has changed
+						//
+						if(liveObjectThis[__loopField.name] != liveObjectThis.__original_data[__loopField.name]) {
+							sqlScript.push('[' + __loopField.name + ']=?');
+							queryParams.push(liveObjectThis[__loopField.name]);
+							changed = true;
+						}
 					}
 				);
 
-				return new modelThis.LiveObject(data);
-			},
-
-
-			buildSelect: function(criteria) {
-				var sqlScript = [];
-				var queryParams = [];
-				
-				sqlScript.push('SELECT ');
-
-				for(var fieldIndex = 0; fieldIndex < modelThis.attachedFields.length; fieldIndex++) {
-					if(fieldIndex > 0) {
-						sqlScript.push(', ');
-					}
-					sqlScript.push('[' + modelThis.attachedFields[fieldIndex].name + ']');
-				}
-
-				sqlScript.push(' FROM [' + modelThis.config.name + ']');
-
-				if(criteria ) {
-					sqlScript.push(' WHERE ');
-
-					var operator;
-					var fieldName;
-					var value;
-
-					for(var criteriaItem in criteria) {
-						fieldName = criteriaItem;
-						operator = '=';
-						value = criteria[criteriaItem];
-						//
-						// double underscores are control codes to modify the operator.
-						// Use them after the fieldname to modify how you want to retrieve the data.
-						//
-						// options are:  lessthan, greaterthan, not, contains
-						//
-						// example:    id__lessthan: 5
-						//
-						// or: name__contains: 'fred'
-						//
-						if(criteriaItem.indexOf('__') != -1) {
-							switch(criteriaItem.split('__')[1].toLowerCase()) {
-								case 'lessthan':
-									operator = '<';
-									break;
-
-								case 'greaterthan':
-									operator = '>';
-									break;
-
-								case 'not':
-									operator = '<>';
-									break;
-
-								case 'contains':
-									operator = ' like ';
-									value = '%' + value + '%';
-									break;
-
-							}
-
-							fieldName = fieldName.split('__')[0];
-						}
-
-						sqlScript.push('[' + fieldName + ']' + operator + '?');
-						queryParams.push(value);
-					}
-				}
-				return [sqlScript.join(' '), queryParams];
-			},
-
-
-			get: function(criteria, successCallback, failureCallback) {
-				var buildSelect = modelThis.manager.buildSelect(criteria);
-				var commandToExecute = buildSelect[0];
-				var queryParams = buildSelect[1];
-				var result = null;
-			
-				modelThis.database.transaction( function(tx) {
-					console.log(commandToExecute);
-					console.log(queryParams);
-
-					tx.executeSql(
-						commandToExecute,
-						queryParams,
-						function(t, data) {
-							if(data.rows.length == 1) {
-								result = new modelThis.LiveObject(data.rows.item(0));
-							}
-							if(data.rows.length > 1) {
-								throw new Exception('Query returned more than one result');
-							}
-						},
-						function(t, error) {
-							
-						}
-					);
-				}, function(a, error) {
-					failureCallback(error);
-				}, function() {
-					successCallback(result);
-				});
-			},
-
-
-			getAll: function(criteria, successCallback, failureCallback) {
-				var buildSelect = modelThis.manager.buildSelect(criteria);
-				var commandToExecute = buildSelect[0];
-				var queryParams = buildSelect[1];
-				var result = [];
-			
-				modelThis.database.transaction( function(tx) {
-					console.log(commandToExecute);
-					console.log(queryParams);
-
-					tx.executeSql(
-						commandToExecute,
-						queryParams,
-						function(t, data) {
-							for(var i = 0; i < data.rows.length; i++) {
-								var newData = new modelThis.LiveObject(data.rows.item(i));
-								result.push(newData);
-							}
-						},
-						function(t, error) {
-							
-						}
-					);
-				}, function(a, error) {
-					failureCallback(error);
-				}, function() {
-					successCallback(result);
-				});
+				sqlScript.push(' WHERE [' + model.config.id + ']=?');
+				queryParams.push(liveObjectThis[model.config.id]);
 			}
+			//
+			// nothing to save
+			//
+			if(!changed) {
+				successCallback();
+				return;
+			}
+
+			var commandToExecute = sqlScript.join('');
+
+			model.database.transaction( function(tx) {
+				console.log(commandToExecute);
+				console.log(queryParams);
+
+				tx.executeSql(
+					commandToExecute,
+					queryParams,
+					function(t, data) {
+						if(data.rowsAffected == 1) {
+							if(data.insertId !== undefined) {
+								liveObjectThis[model.config.id] = data.insertId;
+							}
+						}
+					},
+					function(t, error) {
+						
+					}
+				);
+			}, function(a, error) {
+				failureCallback(error);
+			}, function() {
+				successCallback();
+			});
 		};
 	}
 };
